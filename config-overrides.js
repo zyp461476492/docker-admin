32b7add439cdf1542d65d9d9884fbc625d7bdeac
张蕴鹏 <13150337230@163.com>
2019-07-12 11:01:00 +0800

    [重构] 使用 umi 脚手架

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { override, fixBabelImports } = require('customize-cra');
-
-module.exports = override(
-    fixBabelImports('imports', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: 'css',
-    })
-);
--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import { LocaleProvider } from 'antd';
-import DockerAssetList from './pages/docker-asset-list/DockerAssetList';
-import zhCN from 'antd/es/locale-provider/zh_CN';
-import './App.css';
-
-function App() {
-  return (
-    <LocaleProvider locale={zhCN}>
-      <DockerAssetList />
-    </LocaleProvider>
-  );
-}
-
-export default App;
+export const dva = {
+  config: {
+    onError(err) {
+      err.preventDefault();
+      console.error(err.message);
+    },
+  },
+};
