5792a6b9a197aff65cc51db763883cf51f58d20e
zhangyp <13150337230@163.com>
2019-08-09 16:37:09 +0800

    [新增] 新增 statusTip 组件

---
+++ src/components/status-tip/statusTip.jsx
@@ -0,0 +1,29 @@
+import style from './status.css';
+import { Tooltip } from 'antd';
+
+const StatusTip = (props) => {
+  let fontStyle = style.normal;
+  switch (props.type) {
+    case 'success':
+      fontStyle = style.success;
+      break;
+    case 'warn':
+      fontStyle = style.warn;
+      break;
+    case 'error':
+      fontStyle = style.error;
+      break;
+    default:
+  }
+  console.log(fontStyle);
+  return (
+    <div className={style.tip}>
+      <Tooltip title={props.desc}>
+        <span>{props.title}</span>
+      </Tooltip>
+      <p className={fontStyle}>{props.value}</p>
+    </div>
+  );
+}
+
+export default StatusTip;
