7b5270cdd8a416263cbd2adfea7f217a25cf4400
zhangyp <13150337230@163.com>
2019-08-16 16:55:48 +0800

    feat: detail page

--- src/pages/detail/$id.jsx
+++ src/pages/detail/$id.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './detail.css';
-import { Skeleton, Alert, Card, Row, Col } from 'antd';
+import { Descriptions, Skeleton, Alert, Card, Row, Col } from 'antd';
 import StatusTip from '../../components/status-tip/statusTip';
 
 class DockerBasicPanel extends React.Component {
   render() {
+    console.log(this.props.dockerInfo);
     let context = <Alert message="信息查询失败" type="error" />;
     if (this.props.dockerInfo && this.props.dockerInfo.Res) {
       context = (
@@ -79,6 +80,163 @@ class DockerBasicPanel extends React.Component {
                 </Col>
               </Row>
             </div>
+            <div>
+              <Row type="flex" className={styles.block}>
+                <Col span={24}>
+                  <Card bordered={false}>
+                    <Descriptions title="基本信息" bordered={true}>
+                      <Descriptions.Item label="Architecture">
+                        {this.props.dockerInfo.Obj.Architecture.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="CgroupDriver">
+                        {this.props.dockerInfo.Obj.CgroupDriver.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="DefaultRuntime">
+                        {this.props.dockerInfo.Obj.DefaultRuntime.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="DockerRootDir">
+                        {this.props.dockerInfo.Obj.DockerRootDir.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Driver">
+                        {this.props.dockerInfo.Obj.Driver.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="ID">
+                        {this.props.dockerInfo.Obj.ID.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="IndexServerAddress">
+                        {this.props.dockerInfo.Obj.IndexServerAddress.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="InitBinary">
+                        {this.props.dockerInfo.Obj.InitBinary.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="KernelVersion">
+                        {this.props.dockerInfo.Obj.KernelVersion.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="LoggingDriver">
+                        {this.props.dockerInfo.Obj.LoggingDriver.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="MemTotal">
+                        {this.props.dockerInfo.Obj.MemTotal.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="NCPU">
+                        {this.props.dockerInfo.Obj.NCPU.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="NEventsListener">
+                        {this.props.dockerInfo.Obj.NEventsListener.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="NFd">
+                        {this.props.dockerInfo.Obj.NFd.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="NGoroutines">
+                        {this.props.dockerInfo.Obj.NGoroutines.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Name">
+                        {this.props.dockerInfo.Obj.Name.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="NoProxy">
+                        {this.props.dockerInfo.Obj.NoProxy.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="OSType">
+                        {this.props.dockerInfo.Obj.OSType.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="OperatingSystem">
+                        {this.props.dockerInfo.Obj.OperatingSystem.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="ServerVersion">
+                        {this.props.dockerInfo.Obj.ServerVersion.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="SystemTime">
+                        {this.props.dockerInfo.Obj.SystemTime.toString()}
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </Card>
+                </Col>
+              </Row>
+
+              <Row type="flex" className={styles.block}>
+                <Col span={24}>
+                  <Card bordered={false}>
+                    <Descriptions title="swarm 信息" bordered={true} column={2}>
+                      <Descriptions.Item label="ControlAvailable">
+                        {this.props.dockerInfo.Obj.Swarm.ControlAvailable.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Error">
+                        {this.props.dockerInfo.Obj.Swarm.Error.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="LocalNodeState">
+                        {this.props.dockerInfo.Obj.Swarm.LocalNodeState.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Managers">
+                        {this.props.dockerInfo.Obj.Swarm.Managers.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="NodeAddr">
+                        {this.props.dockerInfo.Obj.Swarm.NodeAddr.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="NodeID">
+                        {this.props.dockerInfo.Obj.Swarm.NodeID.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Nodes">
+                        {this.props.dockerInfo.Obj.Swarm.Nodes.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="RemoteManagers">
+                        {this.props.dockerInfo.Obj.Swarm.RemoteManagers}
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </Card>
+                </Col>
+              </Row>
+
+              <Row type="flex" className={styles.block}>
+                <Col span={24}>
+                  <Card bordered={false}>
+                    <Descriptions title="其他参数" bordered={true} column={2} >
+                      <Descriptions.Item label="BridgeNfIp6tables">
+                        {this.props.dockerInfo.Obj.BridgeNfIp6tables.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="BridgeNfIptables">
+                        {this.props.dockerInfo.Obj.BridgeNfIptables.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="CPUSet">
+                        {this.props.dockerInfo.Obj.CPUSet.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="CPUShares">
+                        {this.props.dockerInfo.Obj.CPUShares.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="CpuCfsPeriod">
+                        {this.props.dockerInfo.Obj.CpuCfsPeriod.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="CpuCfsQuota">
+                        {this.props.dockerInfo.Obj.CpuCfsQuota.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Debug">
+                        {this.props.dockerInfo.Obj.Debug.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="ExperimentalBuild">
+                        {this.props.dockerInfo.Obj.ExperimentalBuild.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="IPv4Forwarding">
+                        {this.props.dockerInfo.Obj.IPv4Forwarding.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="KernelMemory">
+                        {this.props.dockerInfo.Obj.KernelMemory.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="LiveRestoreEnabled">
+                        {this.props.dockerInfo.Obj.LiveRestoreEnabled.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="MemoryLimit">
+                        {this.props.dockerInfo.Obj.MemoryLimit.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="OomKillDisable">
+                        {this.props.dockerInfo.Obj.OomKillDisable.toString()}
+                      </Descriptions.Item>
+                      <Descriptions.Item label="SwapLimit">
+                        {this.props.dockerInfo.Obj.SwapLimit.toString()}
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
           </div>
         </div>
       );
@@ -88,7 +246,6 @@ class DockerBasicPanel extends React.Component {
 }
 
 function mapStateToProps(state) {
-  console.log(state);
   return {
     dockerInfo: state.dockerBasic.dockerInfo,
     loading: state.loading.models.dockerBasic,
