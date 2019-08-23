651e577dc1f3ff9df7017c0edad92f04b4d32e03
zhangyp <13150337230@163.com>
2019-08-23 17:17:31 +0800

    feat: index page refactor

--- src/components/basicTabPanel/tabPanel.jsx
+++ src/components/basicTabPanel/tabPanel.jsx
@@ -1,23 +1,55 @@
 import React from 'react';
-import styles from './common.css';
-import { Icon, Button, Tabs} from 'antd';
+import styles from './panel.css';
+import { Icon, Button, Tabs } from 'antd';
 
-export default class DockerBasicTab extends React.Component {
+const TabPane = { Tabs };
+
+export default class DockerBasicTabe extends React.Component {
   render() {
     const operations = <Button>返回</Button>;
     const IconFont = Icon.createFromIconfontCN({
       scriptUrl: '//at.alicdn.com/t/font_1363529_nopo1vjcec.js',
     });
     return (
-      <div>
+        <div>
         <div className={styles.header}>
-          <Tabs defaultActiveKey={this.props.defaultActiveKey} tabBarExtraContent={operations} onChange={this.tabChange}>
-            {this.props.tabContext}
+          <Tabs defaultActiveKey="basic" tabBarExtraContent={operations} >
+            <TabPane
+              tab={
+                <span>
+                  <IconFont type="icon-Docker" />
+                  基本信息
+                </span>
+              }
+              key="basic"
+            >
+              {this.props.test}
+            </TabPane>
+            <TabPane
+              tab={
+                <span>
+                  <IconFont type="icon-acrrongqijingxiangfuwu" />
+                  镜像
+                </span>
+              }
+              key="image"
+            >
+             
+            </TabPane>
+
+            <TabPane
+              tab={
+                <span>
+                  <IconFont type="icon-rongqifuwuContainerServi" />
+                  容器
+                </span>
+              }
+              key="container"
+            />
           </Tabs>
         </div>
       </div>
     );
   }
 }
-
 
