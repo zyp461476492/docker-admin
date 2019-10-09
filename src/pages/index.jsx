9a81b321676c79b26be3faae84df78877a9e4183
zhangyp <13150337230@163.com>
2019-10-09 14:55:42 +0800

    adjust home style

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './index.css';
 import { connect } from 'dva';
-import { List, Button } from 'antd';
+import { Card, List, Button } from 'antd';
 import router from 'umi/router';
 
 const pageSize = 5;
@@ -37,32 +37,33 @@ class HomePage extends React.Component {
     };
 
     return (
-      <div>
-        <div className={style.home_header}>
-          <Button style={{ margin: '5px' }} onClick={this.goConfigPage}>
+      <Card
+        className={style.home_body}
+        bordered={false}
+        extra={
+          <Button icon="setting" style={{ margin: '5px' }} onClick={this.goConfigPage}>
             配置
           </Button>
-        </div>
-        <div className={style.home_body}>
-          <List
-            style={{ padding: 10 }}
-            itemLayout="horizontal"
-            loading={this.props.loading}
-            dataSource={this.props.dataSource}
-            pagination={pagination}
-            renderItem={item => (
-              <List.Item
-                actions={[<Button onClick={this.goDetailPage.bind(this, item.id)}>详情</Button>]}
-              >
-                <List.Item.Meta
-                  title={<a href="https://ant.design">{item.assetName}</a>}
-                  description={`ip: ${item.ip} port: ${item.port} version: ${item.version}`}
-                />
-              </List.Item>
-            )}
-          />
-        </div>
-      </div>
+        }
+      >
+        <List
+          style={{ padding: 10 }}
+          itemLayout="horizontal"
+          loading={this.props.loading}
+          dataSource={this.props.dataSource}
+          pagination={pagination}
+          renderItem={item => (
+            <List.Item
+              actions={[<Button onClick={this.goDetailPage.bind(this, item.id)}>详情</Button>]}
+            >
+              <List.Item.Meta
+                title={<a href="https://ant.design">{item.assetName}</a>}
+                description={`ip: ${item.ip} port: ${item.port} version: ${item.version}`}
+              />
+            </List.Item>
+          )}
+        />
+      </Card>
     );
   }
 }
